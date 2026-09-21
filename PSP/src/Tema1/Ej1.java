262daa8a4836956182f94e5caeb267b2df174bc2
clase <ikerlozanoromero@gmail.com>
2026-09-21 16:34:24 +0200

    Script clase

--- PSP/src/Tema1/Ej1.java
+++ PSP/src/Tema1/Ej1.java
@@ -1,9 +1,16 @@
 package Tema1;
 
+import java.io.IOException;
+
 public class Ej1 {
 
-    public static void main() {
+    public static void main() throws IOException {
 
+        ProcessBuilder pb = new ProcessBuilder("C:\\Program Files\\Notepad++\\notepad++.exe", "C:\\Users\\dam2\\Documents\\texto.txt\"");
+        Process p = pb.start();
+
+
+        //el segudno parametro es lo que queremos abrir
 
 
 
