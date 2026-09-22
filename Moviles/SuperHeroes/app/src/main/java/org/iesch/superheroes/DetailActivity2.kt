1cff0cef85ccc12ca973e165297263432f6c9fdc
clase <ikerlozanoromero@gmail.com>
2026-09-22 20:24:29 +0200

    Script clase

--- Moviles/SuperHeroes/app/src/main/java/org/iesch/superheroes/DetailActivity2.kt
+++ Moviles/SuperHeroes/app/src/main/java/org/iesch/superheroes/DetailActivity2.kt
@@ -1,6 +1,8 @@
 package org.iesch.superheroes
 
 import android.os.Bundle
+import android.widget.RatingBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,20 @@ class DetailActivity2 : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        //Ultimo paso: recibinos los datos del MiaNActivity
+        val datosRecibidos = intent.extras!!
+
+        val superHeroName = datosRecibidos.getString("superHeroName") ?: "No hay nombre" //esto  no va pq no es un valor nulo, es un valor vacio
+        val alterEgo = datosRecibidos.getString("alterEgo") ?: "No hay alter ego"
+        val bio = datosRecibidos.getString("bio") ?: "No hay biografia"
+        val power = datosRecibidos.getFloat("power")
+
+        //rellenamos los campos con los valores recibidos
+        findViewById<TextView>(R.id.heroName).text = superHeroName
+        findViewById<TextView>(R.id.alterEgo_result).text = alterEgo
+        findViewById<TextView>(R.id.Biografia).text = bio
+        findViewById<RatingBar>(R.id.ratingResult).rating = power
     }
 }
--- Moviles/SuperHeroes/app/src/main/java/org/iesch/superheroes/MainActivity.kt
+++ Moviles/SuperHeroes/app/src/main/java/org/iesch/superheroes/MainActivity.kt
@@ -1,6 +1,11 @@
 package org.iesch.superheroes
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Telephony
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RatingBar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +21,43 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //Aprtir de aqui inrtoduzco el codigo necesario
+
+
+        //1.
+        var botonGuardar = findViewById<Button>(R.id.btn_guardar)
+
+        botonGuardar.setOnClickListener {
+            //Obtnemos los valores de los campos al momento de hacer clik
+            val superHeroName = findViewById<EditText>(R.id.hero_name_edit).text.toString() //por si metenumeros o lo que sea alo pasamos a String
+            val alterEgo = findViewById<EditText>(R.id.alter_ego_edit).text.toString() //por si metenumeros o lo que sea alo pasamos a String
+            val bio = findViewById<EditText>(R.id.bioEdit).text.toString() //por si metenumeros o lo que sea alo pasamos a String
+            val power = findViewById<RatingBar>(R.id.power).rating //por si metenumeros o lo que sea alo pasamos a String
+
+
+            //que quiero hacer cuando pulso el boton "Guardar"
+
+            irADetailActivity(superHeroName, alterEgo, bio, power)
+
+
+
+        }
+
+    }
+
+    fun irADetailActivity(superHeroName: String, alterEgo: String, bio: String, power: Float) {
+
+        //creamos el objeto Intent
+        val intent = Intent(this, DetailActivity2::class.java)
+        //añadimos todos los campos con el metodo putExtra()
+        intent.putExtra("superHeroName", superHeroName)
+        intent.putExtra("alterEgo", alterEgo)
+        intent.putExtra("bio", bio)
+        intent.putExtra("power", power)
+        //de esta manera todos estos datos se enviaran al DetailActivity
+        //lo ejecuto
+        startActivity(intent)
+
     }
 }
